2038be021f8ade6ca3612be8b127146e56941782
mutfakyapimemre <emrekilic@mutfakyapim.com>
2021-03-10 00:12:51 +0300

    uploads folder added

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,63 @@
+<?php return array (
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'fruitcake/laravel-cors' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fruitcake\\Cors\\CorsServiceProvider',
+    ),
+  ),
+  'intervention/image' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Intervention\\Image\\ImageServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Image' => 'Intervention\\Image\\Facades\\Image',
+    ),
+  ),
+  'jenssegers/mongodb' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Jenssegers\\Mongodb\\MongodbServiceProvider',
+      1 => 'Jenssegers\\Mongodb\\MongodbQueueServiceProvider',
+    ),
+  ),
+  'laravel/scout' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Scout\\ScoutServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'laravel/ui' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Ui\\UiServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+);
---
+++ storage/framework/views/b61093b50bd6124b92bba27698bdb8bc936abaed.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Diyetisyen Klinik</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css">
+    <style>
+        body{
+            background: linear-gradient(90deg, rgb(198 196 234) 0%, rgb(24 187 150) 35%, rgb(68 111 119) 100%);
+        }
+
+    </style>
+</head>
+<body>
+<div class="container h-100">
+    <div class="row h-100 justify-content-center align-self-center">
+        <div class="col-12 my-auto py-auto align-middle text-center">
+            <img src="<?php echo e(asset("uploads/logo/diyetisyen-klinik-1600961913.png")); ?>" alt="" class="img-fluid">
+        </div>
+    </div>
+</div>
+</body>
+</html>
+<?php /**PATH /var/www/vhosts/klinikdiyetisyen.com/api.klinikdiyetisyen.com/resources/views/index.blade.php ENDPATH**/ ?>
